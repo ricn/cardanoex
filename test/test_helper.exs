141d58123ced1dfa19adfa95cc9de6f3ac7f8a86
Richard Nyström <ricn@rny.io>
2021-04-10 23:58:35 +0200

    Refactor

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -5,8 +5,72 @@ ExUnit.after_suite(fn _ ->
 
   Enum.each(all_wallets, fn w ->
     if w.id != "5c70f4f4970cadb7d5ec927e634be355df964b52" do
-      IO.inspect(w.id)
       Cardano.Wallet.delete(w.id)
     end
   end)
 end)
+
+defmodule TestHelpers do
+  @wallet_id "5c70f4f4970cadb7d5ec927e634be355df964b52"
+  alias Cardano.Wallet
+  require Logger
+
+  def setup_wallet_with_funds do
+    {:ok, wallet} =
+      case Wallet.fetch(@wallet_id) do
+        {:ok, wallet} ->
+          {:ok, wallet}
+
+        {:error, _} ->
+          Wallet.create_wallet(wallet_attrs())
+      end
+
+    wait_for_synced_wallet(wallet)
+  end
+
+  defp wait_for_synced_wallet(wallet) do
+    status = wallet.state.status
+
+    if status == "syncing" do
+      Logger.info(wallet.state)
+      :timer.sleep(5000)
+      {:ok, wallet} = Wallet.fetch(@wallet_id)
+      wait_for_synced_wallet(wallet)
+    end
+
+    wallet
+  end
+
+  defp wallet_attrs do
+    [
+      name: "wallet_with_funds",
+      mnemonic_sentence: [
+        "bulk",
+        "alley",
+        "fix",
+        "bonus",
+        "vendor",
+        "exchange",
+        "picture",
+        "slam",
+        "autumn",
+        "multiply",
+        "cool",
+        "safe",
+        "maze",
+        "bean",
+        "tourist",
+        "drastic",
+        "rent",
+        "friend",
+        "alcohol",
+        "focus",
+        "invite",
+        "save",
+        "usage",
+        "maid"
+      ],
+      passphrase: "Super_Sekret3.14!"
+    ]
+  end
+end
--- test/transaction_test.exs
+++ test/transaction_test.exs
@@ -1,75 +1,15 @@
 defmodule Cardano.TransactionTest do
   use ExUnit.Case
   doctest Cardano.Transaction
-
   alias Cardano.Transaction
-  alias Cardano.Wallet
-  require Logger
-
-  @wallet_id "5c70f4f4970cadb7d5ec927e634be355df964b52"
+  alias TestHelpers
 
   setup_all do
-    {:ok, wallet} =
-      case Wallet.fetch(@wallet_id) do
-        {:ok, wallet} ->
-          {:ok, wallet}
-
-        {:error, _} ->
-          Wallet.create_wallet(wallet_attrs)
-      end
-
-    [wallet: wait_for_synced_wallet(wallet)]
-  end
-
-  defp wait_for_synced_wallet(wallet) do
-    status = wallet.state.status
-
-    if status == "syncing" do
-      Logger.info(wallet.state)
-      :timer.sleep(5000)
-      {:ok, wallet} = Wallet.fetch(@wallet_id)
-      wait_for_synced_wallet(wallet)
-    end
-
-    wallet
-  end
-
-  def wallet_attrs do
-    [
-      name: "wallet_with_funds",
-      mnemonic_sentence: [
-        "bulk",
-        "alley",
-        "fix",
-        "bonus",
-        "vendor",
-        "exchange",
-        "picture",
-        "slam",
-        "autumn",
-        "multiply",
-        "cool",
-        "safe",
-        "maze",
-        "bean",
-        "tourist",
-        "drastic",
-        "rent",
-        "friend",
-        "alcohol",
-        "focus",
-        "invite",
-        "save",
-        "usage",
-        "maid"
-      ],
-      passphrase: "Super_Sekret3.14!"
-    ]
+    [wallet: TestHelpers.setup_wallet_with_funds()]
   end
 
   describe "estimate fee" do
     test "estimate fee successfully", %{wallet: wallet} do
-
       transaction = %{
         payments: [
           %{
@@ -81,6 +21,7 @@ defmodule Cardano.TransactionTest do
       }
 
       {:ok, estimated_fees} = Transaction.estimate_fee(wallet.id, transaction)
+      assert estimated_fees != nil
     end
   end
 end
