d4c6bfbfa2a8b7996a451fa6cdb26024cab647dc
Richard Nyström <ricn@rny.io>
2021-05-02 22:26:28 +0200

    Do not run wallet clean up on Github Actions

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,13 +1,15 @@
 ExUnit.start()
 
 ExUnit.after_suite(fn _ ->
-  {:ok, all_wallets} = Cardanoex.Wallet.list()
+  if !System.get_env("GITHUB_ACTIONS") do
+    {:ok, all_wallets} = Cardanoex.Wallet.list()
 
-  Enum.each(all_wallets, fn w ->
-    if w.id != "5c70f4f4970cadb7d5ec927e634be355df964b52" do
-      Cardanoex.Wallet.delete(w.id)
-    end
-  end)
+    Enum.each(all_wallets, fn w ->
+      if w.id != "5c70f4f4970cadb7d5ec927e634be355df964b52" do
+        Cardanoex.Wallet.delete(w.id)
+      end
+    end)
+  end
 end)
 
 defmodule TestHelpers do
